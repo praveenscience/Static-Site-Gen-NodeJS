4abaeaf1392bd6efbc807c7cdd236b9b82125a87
Praveen Kumar <praveenscience@gmail.com>
2020-09-20 12:20:45 +0100

    Added Headers and Footers and made to write HTML.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 // Read the configuration.
 const content = require("./content.json");
 const genContentHTML = require("./utils/gen-content");
+const { Header, Footer } = require("./utils/gen-skel");
 // Welcome message.
 console.log("Running Static Site Generator.");
 // Create a directory if it doesn't exist.
@@ -9,5 +10,14 @@ const dir = __dirname + "/build";
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
+
+const finalContent = [];
+console.log("Generating the header.");
+finalContent.push(Header);
 console.log("Generating HTMLs from the content.");
-genContentHTML(content);
+finalContent.push(genContentHTML(content));
+console.log("Generating the footer.");
+finalContent.push(Footer);
+console.log("Writing final HTML file.");
+fs.writeFileSync(__dirname + "/build/index.html", finalContent.join("\n"));
+console.log("Writing final HTML file done.");
