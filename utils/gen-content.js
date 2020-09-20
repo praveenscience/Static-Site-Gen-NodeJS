7e576a3e59ecc9f06eaeb74d10f8b9ed49c9157c
Praveen Kumar <praveenscience@gmail.com>
2020-09-20 12:20:24 +0100

    Updated the spaces correctly.

--- utils/gen-content.js
+++ utils/gen-content.js
@@ -1,11 +1,11 @@
 const genItemHTML = require("./gen-item");
 
 const genContentHTML = content => {
-  const contentHTML = [`<ul class="events">`];
+  const contentHTML = [`    <ul class="events">`];
   console.log(`Found ${content.length} items to process.`);
   const contentsArray = [...content].map(genItemHTML);
-  contentHTML.push("\t" + contentsArray.join("\n\t"));
-  contentHTML.push("</ul>");
+  contentHTML.push(contentsArray.join("\n"));
+  contentHTML.push("    </ul>");
   console.log("HTML Content Generated!");
   return contentHTML.join("\n");
 };
--- utils/gen-item.js
+++ utils/gen-item.js
@@ -1,12 +1,12 @@
 const genItemHTML = item => {
-  return `<li class="event-item">
-		<figure>
-			<a href="${item.Link}">
-				<img src="${item.Img}" alt="${item.Title}" />
-			</a>
-		</figure>
-		<h3><a href="${item.Link}">${item.Title}</a></h3>
-		<p>${item.Desc}</p>
-	</li>`;
+  return `			<li class="event-item">
+				<figure>
+					<a href="${item.Link}">
+						<img src="${item.Img}" alt="${item.Title}" />
+					</a>
+				</figure>
+				<h3><a href="${item.Link}">${item.Title}</a></h3>
+				<p>${item.Desc}</p>
+			</li>`;
 };
 module.exports = genItemHTML;
