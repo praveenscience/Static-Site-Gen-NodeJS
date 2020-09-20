7c4b4d9c2345510f07301a320c14b21663fa38a0
Praveen Kumar <praveenscience@gmail.com>
2020-09-20 11:29:22 +0100

    Created directory if it doesn't exist.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hey! Welcome to Static Site Generator.");
+const fs = require("fs");
+// Welcome message.
+console.log("Running Static Site Generator.");
+// Create a directory if it doesn't exist.
+const dir = __dirname + "/build";
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
