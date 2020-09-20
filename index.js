f770c55b261763f08928f85a6c962b8878aa5e84
Praveen Kumar <praveenscience@gmail.com>
2020-09-20 11:50:17 +0100

    Included Content Gen in index.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 // Read the configuration.
 const content = require("./content.json");
+const genContentHTML = require("./utils/gen-content");
 // Welcome message.
 console.log("Running Static Site Generator.");
 // Create a directory if it doesn't exist.
@@ -8,5 +9,5 @@ const dir = __dirname + "/build";
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
-console.log("Here's the content:");
-console.log(content);
+console.log("Generating HTMLs from the content.");
+genContentHTML(content);
