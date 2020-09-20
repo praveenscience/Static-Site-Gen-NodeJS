d70306d51c8b6c5a599091edac01b871875f6171
Praveen Kumar <praveenscience@gmail.com>
2020-09-20 12:03:39 +0100

    Added the HTML for each Item.

--- utils/gen-item.js
+++ utils/gen-item.js
@@ -1,4 +1,12 @@
 const genItemHTML = item => {
-  return `<li class="event-item">${item.Title}</li>`;
+  return `<li class="event-item">
+		<figure>
+			<a href="${item.Link}">
+				<img src="${item.Img}" alt="${item.Title}" />
+			</a>
+		</figure>
+		<h3><a href="${item.Link}">${item.Title}</a></h3>
+		<p>${item.Desc}</p>
+	</li>`;
 };
 module.exports = genItemHTML;
