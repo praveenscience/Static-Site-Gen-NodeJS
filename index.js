8b2876f367656cceba5d7a0938e2f6266fe08b50
Praveen Kumar <praveenscience@gmail.com>
2020-09-20 11:35:39 +0100

    Reading content from JSON.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+// Read the configuration.
+const content = require("./content.json");
 // Welcome message.
 console.log("Running Static Site Generator.");
 // Create a directory if it doesn't exist.
@@ -6,3 +8,5 @@ const dir = __dirname + "/build";
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
+console.log("Here's the content:");
+console.log(content);
